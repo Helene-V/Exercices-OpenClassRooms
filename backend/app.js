0fc2acd09fca8ddad4fbfb71e1346e2799933ab7
Ln <rainbowliketravel@gmail.com>
2021-05-06 15:37:00 +0200

    préparation authentification des utilisateurs

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const stuffRoutes = require('./routes/stuff');
+const userRoutes = require('./routes/user');
 
 mongoose.connect('mongodb+srv://TestLN:TestLN@cluster12.rnovz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
   { useNewUrlParser: true,
@@ -22,5 +23,6 @@ app.use((req, res, next) => {
 app.use(bodyParser.json());
 
 app.use('/api/stuff', stuffRoutes);
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
