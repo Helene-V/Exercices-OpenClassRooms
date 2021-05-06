6eb82064740ca94c7ba8de355fa120f66ee5cb8e
Ln <rainbowliketravel@gmail.com>
2021-05-06 15:29:00 +0200

    préparation authentification des utilisateurs

---
+++ backend/models/User.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+const userSchema = mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
+});
+
+userSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model('User', userSchema);
+
+// unique permet de ne pas s'enregistrer à plusieurs reprises avec la même adresse mail
